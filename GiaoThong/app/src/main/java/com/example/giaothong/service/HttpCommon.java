455abc5bcd859bf9ab0e4fab2236111b039cf96a
phuc <nguyendoantanphuc@gmail.com>
2021-04-03 10:42:15 +0700

    sadfsdf

--- GiaoThong/app/src/main/java/com/example/giaothong/service/HttpCommon.java
+++ GiaoThong/app/src/main/java/com/example/giaothong/service/HttpCommon.java
@@ -8,7 +8,6 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.example.giaothong.ui.custom.TakePhotoActivity;
 
 import org.json.JSONObject;
 
@@ -17,7 +16,7 @@ public class HttpCommon {
     public Object activity;
 
     public void postMethod(String url, JSONObject object) {
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "http://192.168.63.49:3000"+url, null, new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "http://192.168.63.49:3000" + url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 Toast.makeText((Context) activity, response.toString(), Toast.LENGTH_SHORT).show();
